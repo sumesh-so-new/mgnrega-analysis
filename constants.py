7fac219ce6e5278b7def64547b104e69090afcc7
sumesh-so-new <sumesh@boomlive.in>
2026-03-18 08:09:59 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,10 +1,29 @@
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
+
+# HOST = os.getenv("HOST")
+# PORT = os.getenv("PORT")
+# DATABASE = os.getenv("DATABASE")
+# USER = os.getenv("USER")
+# PASSWORD = os.getenv("PASSWORD")
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")a
+
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from streamlit import st
 
-HOST = os.getenv("HOST")
-PORT = os.getenv("PORT")
-DATABASE = os.getenv("DATABASE")
-USER = os.getenv("USER")
-PASSWORD = os.getenv("PASSWORD")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# HOST = os.getenv("HOST")
+# PORT = os.getenv("PORT")
+# DATABASE = os.getenv("DATABASE")
+# USER = os.getenv("USER")
+# PASSWORD = os.getenv("PASSWORD")
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+HOST = st.secrets["HOST"]
+PORT = st.secrets["PORT"]
+DATABASE = st.secrets["DATABASE"]
+USER =  st.secrets["USER"]
+PASSWORD = st.secrets["PASSWORD"]
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
