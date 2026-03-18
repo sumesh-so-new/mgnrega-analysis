09087d5ee13b90413533275775d057583ee0adc1
sumesh-so-new <sumesh@boomlive.in>
2026-03-18 11:59:44 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,17 +1,17 @@
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
-
-# HOST = os.getenv("HOST")
-# PORT = os.getenv("PORT")
-# DATABASE = os.getenv("DATABASE")
-# USER = os.getenv("USER")
-# PASSWORD = os.getenv("PASSWORD")
-# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")a
-
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+DATABASE = os.getenv("DATABASE")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
 # from streamlit import st
 
 # HOST = os.getenv("HOST")
@@ -21,9 +21,9 @@ load_dotenv()
 # PASSWORD = os.getenv("PASSWORD")
 # OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-HOST = st.secrets["HOST"]
-PORT = "5432"
-DATABASE = st.secrets["DATABASE"]
-USER =  st.secrets["USER"]
-PASSWORD = st.secrets["PASSWORD"]
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+# HOST = st.secrets["HOST"]
+# PORT = "5432"
+# DATABASE = st.secrets["DATABASE"]
+# USER =  st.secrets["USER"]
+# PASSWORD = st.secrets["PASSWORD"]
+# OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
